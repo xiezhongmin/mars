dd72090b698c834bffbc39cd73cd6f5f0734fe90
duke <admin@admindeMacBook-Pro-4.local>
2022-06-27 10:56:46 +0800

    Update

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "b1318491"
-#define MARS_PATH "master"
+#define MARS_REVISION "ae5ddb7f"
+#define MARS_PATH "dklogger"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2022-06-24 17:20:34"
+#define MARS_BUILD_TIME "2022-06-24 19:09:15"
 #define MARS_TAG ""
 
 #endif
